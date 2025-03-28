1d2571070ecd9b27bd8b6af5a93463bb7447100d
ldd2001 <56123971+ldd2001@users.noreply.github.com>
2025-03-28 21:31:28 +0800

    6.4.3

--- practice/6_dict/6.4.3.py
+++ practice/6_dict/6.4.3.py
@@ -1,10 +1,25 @@
-pizza = {
-    'crust':'thick',
-    'toppings':['mushroom','extra cheese']
-    }
+#在字典中存列表 ； 一个key关联多个value时
 
-print(pizza['crust'])
-print(pizza['toppings'])
+favorite_languages = {
+    'jen':['python','c++'],
+    'sarah':['c'],
+    'edward':['rust','fortan'],
+    'phile':['c','c++']
+}
 
-for topping in pizza['toppings']:
-    print(topping)
+for name, languages in favorite_languages.items():            # key values 一定要对应字典
+    if len(languages) > 1:
+        print(f"{name.title()}'s favorite languages are:")        #  
+        for language in languages:                                # 对应list
+            print(f"{language.title()}")
+    else:
+        print(f"{name.title()}'s favorite languages is:")        #  
+        for language in languages:                                # 对应list
+            print(f"{language.title()}")
+
+            
+#想想为什么下面的代码不对
+for name, languages in favorite_languages.items():
+    print(f"{name.title()}'s favorite languages are:")
+    for language in favorite_languages.values():   #问题出在这：。title用于字符串，favorite_langues.values他是一个列表，是不可以用.title的
+        print(f"{language.title()}")               #所以，for name languages里面，language就是列表，所以，12行，就是从列表里提取字符串
